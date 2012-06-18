8fca9fe1ac171b5c0254ad6a8e1b8fea624cb485
Steve Sanbeg <ssanbeg@users.sourceforge.net>
2012-06-17 21:49:24 -0400

    add onrelease/onpause to make button press more responsive

--- src/com/nfshost/sanbeg/flashlight/FlashLightActivity.java
+++ src/com/nfshost/sanbeg/flashlight/FlashLightActivity.java
@@ -1,17 +1,15 @@
 package com.nfshost.sanbeg.flashlight;
 
-import java.io.IOException;
-
 import android.app.Activity;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class FlashLightActivity extends Activity {
     private Camera camera=null;
-
+    private Camera.Parameters camera_parameters;
+    String flash_mode;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,24 +17,33 @@ public class FlashLightActivity extends Activity {
         setContentView(R.layout.main);
 
     }
-    public void onToggleClicked(View v) {
-    	if (camera==null) camera=Camera.open();
+    
+    @Override
+    public void onResume(){
+    	super.onResume();
+    	if (camera==null)
+    		camera=Camera.open();
 
-     	// Perform action on clicks
-    	Camera.Parameters parameters = camera.getParameters();
+    	camera_parameters = camera.getParameters();
+    	flash_mode = camera_parameters.getFlashMode();
+    	camera_parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+    	camera.setParameters(camera_parameters);
+    }
+    @Override
+    public void onPause(){
+    	super.onPause();
+    	camera_parameters.setFlashMode(flash_mode);
+    	camera.setParameters(camera_parameters);
+        camera.release();
+        camera=null;	
+    }
+    public void onToggleClicked(View v) {
         if (((ToggleButton) v).isChecked()) {
-        	parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            Toast.makeText(this, "Toggle on", Toast.LENGTH_SHORT).show();
-            camera.setParameters(parameters);
+         	camera.setParameters(camera_parameters);
             camera.startPreview();
 
         } else {
-        	parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
-            Toast.makeText(this, "Toggle off", Toast.LENGTH_SHORT).show();
-            camera.setParameters(parameters);
             camera.stopPreview();
-            camera.release();
-            camera=null;
         }
     }
     
