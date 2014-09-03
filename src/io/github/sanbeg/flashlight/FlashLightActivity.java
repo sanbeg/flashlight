f6c70d3b7262cd8b6486b6327c68e914e168f0ef
Steve Sanbeg <steve.sanbeg@gmail.com>
2014-09-02 20:47:17 -0400

    reformat

--- src/io/github/sanbeg/flashlight/FlashLightActivity.java
+++ src/io/github/sanbeg/flashlight/FlashLightActivity.java
@@ -6,40 +6,38 @@ import android.view.View;
 import android.widget.ToggleButton;
 import io.github.sanbeg.flashlight.R;
 
-
 public class FlashLightActivity extends Activity {
     private Flash flash = new Flash();
-    
 
-    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-     }
-    
+    }
+
     @Override
-    public void onResume(){
-    	super.onResume();
-    	flash.open();
-    	 	
+    public void onResume() {
+        super.onResume();
+        flash.open();
+
         ToggleButton the_button = (ToggleButton) findViewById(R.id.flashlightButton);
-        if (the_button.isChecked()){
-        	flash.on();
+        if (the_button.isChecked()) {
+            flash.on();
             the_button.setKeepScreenOn(true);
         }
     }
-    
+
     @Override
-    public void onPause(){
-    	super.onPause();
-		flash.close();
+    public void onPause() {
+        super.onPause();
+        flash.close();
     }
-    public void onToggleClicked(View v) {   	
+
+    public void onToggleClicked(View v) {
         if (((ToggleButton) v).isChecked()) {
-        	flash.on();
+            flash.on();
             v.setKeepScreenOn(true);
         } else {
             flash.off();
