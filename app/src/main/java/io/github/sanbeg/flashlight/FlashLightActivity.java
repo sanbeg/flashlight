4fb0bfc03d13508fd41a2b3a1c2a80f3e8625c6b
Steve Sanbeg <steve.sanbeg@gmail.com>
2015-06-12 22:23:06 -0400

    release camera when turning off, change background between white & black when off/off, long press on background to toggle

--- app/src/main/java/io/github/sanbeg/flashlight/FlashLightActivity.java
+++ app/src/main/java/io/github/sanbeg/flashlight/FlashLightActivity.java
@@ -1,30 +1,36 @@
 package io.github.sanbeg.flashlight;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ToggleButton;
 
 public class FlashLightActivity extends Activity {
-    private Flash flash = new Flash();
+    private final Flash flash = new Flash();
+    private View background;
+    private ToggleButton the_button;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
+        background = findViewById(R.id.backgroundLayout);
+        background.setOnLongClickListener(new LongClickListener());
+        the_button = (ToggleButton) findViewById(R.id.flashlightButton);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        flash.open();
-
-        ToggleButton the_button = (ToggleButton) findViewById(R.id.flashlightButton);
+        //flash.open();
         if (the_button.isChecked()) {
             flash.on();
             the_button.setKeepScreenOn(true);
+            background.setBackgroundColor(Color.WHITE);
+        } else {
+            background.setBackgroundColor(Color.BLACK);
         }
     }
 
@@ -36,12 +42,25 @@ public class FlashLightActivity extends Activity {
 
     public void onToggleClicked(View v) {
         if (((ToggleButton) v).isChecked()) {
+            flash.open();
             flash.on();
             v.setKeepScreenOn(true);
+            background.setBackgroundColor(Color.WHITE);
         } else {
-            flash.off();
+            //flash.off();
+            flash.close();
             v.setKeepScreenOn(false);
+            background.setBackgroundColor(Color.BLACK);
         }
     }
 
+    public class LongClickListener implements View.OnLongClickListener {
+        @Override
+        public boolean onLongClick(View view){
+            //view.setBackgroundColor(Color.WHITE);
+            the_button.setChecked(!the_button.isChecked());
+            onToggleClicked(the_button);
+            return true;
+        }
+    }
 }
